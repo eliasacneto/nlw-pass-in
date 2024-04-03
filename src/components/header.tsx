c5042de7d59bd6a10ca8247ac5e34bdbbec804ab
Elias Neto <netoeac.dev@gmail.com>
2024-04-03 16:59:07 -0300

    ui created using tailwindcss

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,12 @@
 import nlwUnitedIcon from '../assets/nlw-unite-icon.svg'
 export function Header() {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className='flex items-center gap-5 py-2'>
+            <img src={nlwUnitedIcon} />
+            <nav className='flex items-center gap-5'>
+                <a href='' className='font-medium text-sm text-zinc-300'>Eventos</a>
+                <a href='' className='font-medium text-sm' >Participantes</a>
+            </nav>
         </div>
     )
 }
